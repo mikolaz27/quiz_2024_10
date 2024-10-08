942ad5ba65add97e68883abdff640d4b51577f0b
Mykhailo Lazoryk <mikolaz2727@gmail.com>
2024-10-08 21:20:08 +0300

    Added settigs

--- src/manage.py
+++ src/manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
     if sys.argv[1].lower() == "test":
         print("NOTE: Running black formatting")
         subprocess.run(["black", "--config", ".black.toml", "."])
